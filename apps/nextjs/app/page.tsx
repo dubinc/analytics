4fbcce020e56b585229207011a7616aff2626008
Kiran K <kiran@dub.co>
2024-05-16 01:36:03 +0530

    fix the demo

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-// import Script from 'next/script';
-import { Analytics as DubAnalytics } from '@dub/analytics/react';
-
 export default function Home() {
-  return (
-    <main>
-      <DubAnalytics />
-    </main>
-  );
+  return <main>Home</main>;
 }
