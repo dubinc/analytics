8714536957286899d2d15e7730e158940f47e4de
Kiran K <kiran@dub.co>
2024-06-17 16:53:18 +0530

    clone the nextjs example

---
+++ apps/nextjs-geolocation-script/app/actions/track.ts
@@ -0,0 +1,11 @@
+'use server';
+
+// Track lead
+export async function trackLead() {
+  console.log('Lead tracked');
+}
+
+// Track sale
+export async function trackSale() {
+  console.log('Sale tracked');
+}
---
+++ apps/nextjs-geolocation-script/app/api/track/sale/route.ts
@@ -0,0 +1,26 @@
+const customerId = 'iGyAA2';
+
+export async function POST() {
+  // TODO:
+  // Use SDK method to track a sale
+
+  await fetch(`${process.env.DUB_API_URL}/track/sale`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${process.env.DUB_API_KEY}`,
+    },
+    body: JSON.stringify({
+      customerId,
+      paymentProcessor: 'stripe',
+      productId: 'super-product-123',
+      amount: 2000,
+      recurring: true,
+      recurringInterval: 'month',
+      recurringIntervalCount: 4,
+      refunded: false,
+    }),
+  });
+
+  return Response.json({ message: 'OK' });
+}
