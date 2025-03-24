2cb960987df2d018e3291cf763231c74e6d5e15b
Kiran K <kiran@dub.co>
2025-03-24 15:08:45 +0530

    Update generic.ts

--- packages/web/src/generic.ts
+++ packages/web/src/generic.ts
@@ -1,10 +1,12 @@
 import { inject } from './inject';
 import type { AnalyticsProps } from './types';
+import { trackClick } from './track-click';
 
-export { inject };
+export { inject, trackClick };
 export type { AnalyticsProps };
 
 // eslint-disable-next-line import/no-default-export -- Default export is intentional
 export default {
   inject,
+  trackClick,
 };
