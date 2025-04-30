a4825e0785f33592082a6b236b4a15ae8b7b88ae
Kiran K <kiran@dub.co>
2025-04-30 12:18:03 +0530

    one more test

--- packages/script/src/base.js
+++ packages/script/src/base.js
@@ -1,5 +1,5 @@
 (function () {
-  console.log('Test the GH action 3');
+  console.log('Test the GH action 4');
 
   // Store script reference for extensions
   const script = document.currentScript;
