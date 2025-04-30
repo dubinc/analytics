defd26fc2a7e4d186fea762c87019a9db9f83603
Kiran K <kiran@dub.co>
2025-04-30 14:12:27 +0530

    Update base.js

--- packages/script/src/base.js
+++ packages/script/src/base.js
@@ -1,5 +1,5 @@
 (function () {
-  console.log('Test the GH action 12');
+  console.log('Test the GH action 13');
 
   // Store script reference for extensions
   const script = document.currentScript;
